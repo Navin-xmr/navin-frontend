5f2ad96af834fabf5c821d60166702ff738822e9
Asher <141028690+No-bodyq@users.noreply.github.com>
2026-02-20 11:40:46 +0100

    feat: add Navbar component to Home page

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Navbar from "@/components/Navbar/Navbar";
 import "./Home.css";
 
 const Home: React.FC = () => {
   return (
     <div className="home-container">
+      <Navbar />
       <div className="home-content">
         <h1 className="home-title">
           Transparent <br /> Tracking Solution
