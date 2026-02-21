123e946fd7f28be0e0b79f1b0bb5f71e0ea42359
caxtonacollins <caxtonacollins@gmail.com>
2026-02-21 15:22:21 +0100

    feat: initialize CreateShipment component skeleton

---
+++ frontend/src/pages/dashboard/Company/CreateShipment/CreateShipment.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './CreateShipment.css';
+
+const CreateShipment: React.FC = () => {
+  return (
+    <div className="create-shipment-container">
+      <h2>Create New Shipment</h2>
+    </div>
+  );
+};
+
+export default CreateShipment;
