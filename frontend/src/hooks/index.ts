d51c7a773bc007cf49e7ed5f5fb030e623fd1872
Musa March <musamarch022005@gmail.com>
2026-07-28 00:21:10 +0100

    feat: add draft autosave and recovery to create shipment form

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -4,4 +4,6 @@ export type { ShortcutDef } from './useKeyboardShortcuts';
 export { useFocusTrap } from './useFocusTrap';
 export { useScrollSpy } from './useScrollSpy';
 export { useBulkSelection } from './useBulkSelection';
+export { useFormDraft } from './useFormDraft';
+export type { FormDraft, UseFormDraftReturn } from './useFormDraft';
 export type { UseBulkSelectionReturn } from './useBulkSelection';
